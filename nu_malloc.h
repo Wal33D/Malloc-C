38b9f136867da73acba748dcc6ebbe7e03144e0d
Waleed Judah <aquataze@yahoo.com>
2025-06-07 03:35:38 -0400

    Add C++ compatibility and improve allocator

--- nu_malloc.h
+++ nu_malloc.h
@@ -5,10 +5,18 @@
 // Included for the `size_t` type.
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Declare function prototypes
 void* nu_malloc(size_t);
 void* nu_calloc(size_t, size_t);
 void* nu_realloc(void*, size_t);
 void nu_free(void*);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
